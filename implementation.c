590e410ae7fefafd76f57ac5880179b725e9a7ed
Mayra Robles <mayrarobles3985@gmail.com>
2024-03-08 19:21:01 -0700

    untested malloc progress

--- implementation.c
+++ implementation.c
@@ -57,6 +57,7 @@
 
 #include <stddef.h>
 #include <sys/mman.h>
+#include <stdlib.h>
 
 /* Predefined helper functions */
 
@@ -158,24 +159,185 @@ static int __try_size_t_multiply(size_t *c, size_t a, size_t b) {
 */
 
 
+
+/* Struct that represents memory blocks implemented as
+  a linked list.*/
+typedef struct memory_block_header {
+    size_t size;
+    int is_free;
+  struct memory_block_header *next;
+} memory_block_header_t;
+
+
+/* This global variable holds the start of the linked list
+   of free memory blocks */
+static free_block_list = NULL;
+
+/* This function checks that there is space left over
+   to at least create a header after the size of memory
+   the user wants is allocated.
+   - Returns 1 if there is enough space for a header
+   - Returns 0 if there is not enought space for a header
+*/
+int check_enough_space_for_header_after_allocation(memory_block_header_t *ptr,
+						   size_t desired_size) {
+  return (*ptr->size - desired_size - sizeof(memory_block_header_t) >= 0); 
+}
+
+
+/* This function makes a header and allocates the memory the user wants.
+   We assume that there is enough space for the memory allocation
+   and for the header.
+   - Returns 1 if the making of the header and allocation was sucessful
+   - Returns 0 if unsuccessful */
+void make_header_and_allocate_memory(memory_block_header_t *start,
+				     size_t desired_size) {
+  memory_block_header_t * new_header;
+  /* Figure out where the new header goes */
+  new_header = start + sizeof(memory_block_header_t) + desired_size;
+
+  /* Initialize its attributes */
+  new_header->size = start->size - sizeof(memory_block_header_t) - desired_size;
+  new_header->is_free = 1;
+  if (start != NULL) {
+    new_header->next = start->next;
+  } else {
+    new_header->next = NULL;
+  }
+    
+  start->size = desired_size; // Update size
+  start->next = new_header;
+}
+
+
+/* This function makes */
+void make_headers_and_allocate_memory_first_allocation(memory_block_header_t *start,
+						       size_t desired_size) {
+  start->size = desired_size;
+  start->is_free = 0;
+
+  new_header = start + sizeof(memory_block_header_t) + desired_size;
+   /* Initialize its attributes */
+  new_header->size = sizeof(memory_block_header_t) + desired_size;
+  new_header->is_free = 1;
+  
+  start->next = new_header;
+
+  new_header->next = NULL;
+}
+
+/*
+  This function returns a ptr to a block of memory
+  of size 'size'.
+   - It allocates new memory using mmap if there is
+     no more space in our linked list to store memory
+     of that size.
+   - If there is space in our linked list, it returns
+     a ptr to the first block in our list that is the
+     beginning of a chunk of at least the size requested.
+*/
+memory_block_header_t get_ptr_next_memory_fit(size_t size) {
+  void  *new_block;
+  memory_block_header_t *cur = free_block_list;
+
+  if (cur == NULL) {
+    /* First time allocating memory */
+    new_block = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE,
+		     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    free_block_list = (memory_block_header_t) new_block;
+    
+    cur = free_block_list;
+    cur->size = getpagesize();
+    cur->is_free = 1;
+    if(check_enough_space_for_header_after_allocation(cur, size)) {
+      make_headers_and_allocate_memory_first_allocation((memory_block_header_t)cur,
+							size);
+      return (cur + sizeof(memory_block_header_t));
+    }
+    return NULL;
+  }
+  
+  /* Iterate through memory blocks linked list */
+  while (cur != NULL) {
+    if (cur->is_free &&
+	check_enough_space_for_header_after_allocation(cur, size)) {
+      /* If the current block of memory is free and the size of the
+	 memory we were asked for is less than available memory, then
+	 we have found continuous memory block of the size we need.
+      */
+      make_header_and_allocate_memory(cur, size);
+      return (cur + sizeof(memory_block_header)); // Do not include the header
+    }
+    // Go to next element in the linked list
+    cur = cur->next;
+  }
+
+  /* After iterating through the entire linked list we see that the size
+     of the memory we were asked for is more than the current available
+     memory,so we need to allocate a fresh chunk of memory.
+  */
+  cur = free_block_list;
+  new_block = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE,
+		   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  cur->size = getpagesize();
+  cur->is_free = 1;
+  
+  while (cur != NULL) {
+    if (cur->next == NULL) {
+      /* We want to insert this new chunk after the last header in
+	 our current linked list.*/
+      cur->next = new_block;
+      make_headers_and_allocate_memory_first_allocation((memory_block_header_t)cur,
+							size);
+    }
+  }
+  
+  
+  return NULL;
+}
+
 /* End of your helper functions */
 
 /* Start of the actual malloc/calloc/realloc/free functions */
 
 void __free_impl(void *);
 
+
 void *__malloc_impl(size_t size) {
-  /* STUB */
-  return NULL;
+  if (size == 0) {
+    /* If the size we want to allocate is zero, do nothing*/
+    return NULL;
+  }
+  
+  return get_ptr_to_next_fit(size);
 }
+
 
 void *__calloc_impl(size_t nmemb, size_t size) {
-  /* STUB */
-  return NULL;  
+  void *allocated_block;
+  size_t *multiplication_result;
+  
+  /* Get total space we need by multiplying nmmeb and size */
+  if (__try_size_t_multiply(multiplication_result, nmemb, size) == 0) {
+    printf("Failed at multiplying numbers of that size.\n");
+    return NULL;  
+  }
+  /* Multiplication was sucessful, now we know how much space to allocate*/
+  allocated_block = __malloc_impl(multiplication_result);
+  
+  if (allocated_block == NULL) {
+    printf("Unable to allocate memory using malloc");
+    return NULL;
+  }
+
+  /* Initialize every byte to zero */
+  __memset(allocated_block, 0, size);
+  return allocated_block;
 }
 
+
 void *__realloc_impl(void *ptr, size_t size) {
-  /* STUB */
+  /* When the size we are asked to reallocate is less */
   return NULL;  
 }
 
